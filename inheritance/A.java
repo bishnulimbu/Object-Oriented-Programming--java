36fdcdf60540e2435e0d4a3f4c90c077479ccdd8
Bishnu Limbu <87610084+bishnulimbu@users.noreply.github.com>
2023-09-30 21:07:16 +0545

    Create A.java

---
+++ inheritance/A.java
@@ -0,0 +1,27 @@
+class Animal{
+    void eat(){
+        System.out.println("Dog is eating.");
+    }
+}
+
+class Dog extends Animal{ 
+  void beat(){
+    System.out.println("Dog is beating.");
+  }
+}
+
+class GermanShepherd extends Dog{
+    void head(){
+    System.out.println("German Shepherd is an expensive dog.");
+    super.eat();
+    beat();
+  }
+}
+
+public class MultiLevelInheritane {
+  public static void main(String[] args) {
+    GermanShepherd obj3 = new GermanShepherd();
+    obj3.head();
+    
+  }
+}
